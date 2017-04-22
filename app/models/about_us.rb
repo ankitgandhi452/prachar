e2f274697ddd7af32043560259bba84ddb90d928
Ankit <ankit@ptotem.com>
2017-04-22 23:33:12 +0530

    header image in about us page

--- app/models/about_us.rb
+++ app/models/about_us.rb
@@ -2,8 +2,8 @@ class AboutUs
 	include Mongoid::Document
 	include Mongoid::Paperclip
 
-	# has_mongoid_attached_file :header_image
-	# validates_attachment_content_type :header_image, content_type: /\Aimage/
+	has_mongoid_attached_file :header_image
+	validates_attachment_content_type :header_image, content_type: /\Aimage/
 	# validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
 
 	field :content, type: String
