f50729dd789b14b01a1c568b36554f4ef25e0348
Ankit <ankit@ptotem.com>
2017-04-22 23:58:24 +0530

    digital media models

--- app/models/digital_media_image.rb
+++ app/models/digital_media_image.rb
@@ -1,8 +1,11 @@
-class DigitalMediaImages
+class DigitalMediaImage
 	include Mongoid::Document
 	include Mongoid::Timestamps
 	include Mongoid::Paperclip
 
 	has_mongoid_attached_file :content_image
 	validates_attachment_content_type :content_image, content_type: /\Aimage/
+	field :sequence, type: Integer
+
+	belongs_to :digital_media
 end
--- app/models/digital_medium.rb
+++ app/models/digital_medium.rb
@@ -1,9 +1,9 @@
-class DigitalMedia
+class DigitalMedium
 	include Mongoid::Document
 	include Mongoid::Timestamps
 
 	field :header_text, type: String
 
-
+	has_many :digital_media_images
 
 end
