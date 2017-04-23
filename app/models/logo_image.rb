3877b9123b9fcb6b63dde6f6464b890b04142929
Ankit <ankit@ptotem.com>
2017-04-23 22:52:54 +0530

    caption in logo image

--- app/models/logo_image.rb
+++ app/models/logo_image.rb
@@ -4,6 +4,7 @@ class LogoImage
 	include Mongoid::Paperclip
 
 	field :sequence, type: Integer, default: ->{LogoImage.count}
+	field :caption, type: String
 	has_mongoid_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage/
 
