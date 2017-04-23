b7cf94ea5128a85ea0c937b03b42ba2a08dea89b
Ankit <ankit@ptotem.com>
2017-04-24 02:40:08 +0530

    pages and models

--- app/controllers/navigation_controller.rb
+++ app/controllers/navigation_controller.rb
@@ -1,17 +1,18 @@
 class NavigationController < ApplicationController
 	# before_filter :set_page_title
+	skip_before_filter :verify_authenticity_token, :only => [:apply]
 	def home
 
 	end
 
 	def logos
 		@logo = Logo.last
-		@logo_images = @logo.logo_images
+		@logo_images = @logo.logo_images.order("ASC sequence")
 	end
 
 	def tvcs
 		@tvc = Tvc.last
-		@tvc_videos = @tvc.tvc_videos
+		@tvc_videos = @tvc.tvc_videos.order("ASC sequence")
 	end
 
 	def about_us
@@ -19,8 +20,50 @@ class NavigationController < ApplicationController
 		@team_members = TeamMember.all
 	end
 
-	# private
-	# def set_page_title
-		# @title =
-	# end
+	def career
+		@career = Career.last
+		@openings = @career.openings.order("ASC sequence")
+		@job_application = JobApplication.new
+	end
+
+	def apply
+		job_application = JobApplication.new(navigation_params)
+		respond_to do |format|
+			if job_application.save
+
+				format.html{
+					flash[:notice] = 'Successfully Applied'
+					redirect_to(:back)
+				} 
+			else
+				format.html{
+					flash[:notice] = 'Unsuccessfully Applied'
+					redirect_to(:back)
+				} 
+			end
+		end
+	end
+
+	def contact_us
+		@contacted = Contacted.new
+		@contact_us = ContactUs.last
+	end
+
+	def contacted
+		
+	end
+
+	private
+	def navigation_params
+
+      if !params[:job_application].nil?
+        params.fetch(:job_application, {}).permit(:name, :email, :subject, :description, :message, :resume)
+      elsif !params[:contacted].nil?
+        params.fetch(:contacted, {}).permit(:name, :email, :subject, :description, :message)
+      else
+      end
+          
+      # params.fetch(:library_course, {}).permit(:name, :description, :sector, :private, :published, :owner_id, :category, :type, course_images_attributes: [:caption, :subcaption, :image] ) if params[:library_course]
+      # params.fetch(:modularized_course, {}).permit(:name, :description, :sector, :private, :published, :owner_id, :category, :type, course_images_attributes: [:caption, :subcaption, :image] ) if params[:modularized_course]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   	get '/logos' => 'navigation#logos', as: :logos, constraints: { format: 'html' }
   end
   get '/about_us' => 'navigation#about_us', as: :about, constraints: { format: 'html' }
+  get '/contact_us' => 'navigation#contact_us', as: :contact_us, constraints: { format: 'html' }
+  get '/career' => 'navigation#career', as: :career, constraints: { format: 'html' }
+  post '/job_application' => 'navigation#apply', as: :job_applications, constraints: { format: 'json' }
+  post '/contact' => 'navigation#apply', as: :contacteds, constraints: { format: 'json' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'navigation#home'
 end
