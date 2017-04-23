4eb4836df0f88a1f19d77b140d794ef805276eb9
Samrith Shankar <samrith@ithaka.travel>
2017-04-24 02:41:46 +0530

    Added def and route for collateral and celeb mgmt

--- app/controllers/navigation_controller.rb
+++ app/controllers/navigation_controller.rb
@@ -9,9 +9,19 @@ class NavigationController < ApplicationController
 		@logo_images = @logo.logo_images
 	end
 
+	def collaterals
+		@collateral = Collateral.last
+		@collateral_images = @collateral.collateral_images
+	end
+
 	def tvcs
 		@tvc = Tvc.last
 		@tvc_videos = @tvc.tvc_videos
+	end
+
+	def celebrity_management
+		@celebrity_management = CelebrityManagement.last
+		@celebrities = @celebrity_management.celebrity_management_images
 	end
 
 	def about_us
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   scope '/work'do
-  	get '/tvcs' => 'navigation#tvcs', as: :tvcs, constraints: { format: 'html' }
   	get '/logos' => 'navigation#logos', as: :logos, constraints: { format: 'html' }
+    get '/collaterals' => 'navigation#collaterals', as: :collaterals, constraints: { format: 'html' }
+    get '/tvcs' => 'navigation#tvcs', as: :tvcs, constraints: { format: 'html' }
   end
+
+  scope '/services' do
+  	get '/celebrity_management' => 'navigation#celebrity_management', as: :celebrity_management, constraints: { format: 'html' }
+  end
+
   get '/about_us' => 'navigation#about_us', as: :about, constraints: { format: 'html' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'navigation#home'
 end
