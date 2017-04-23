59de45d71ea9d35216b656d37f487371beb93918
Ankit <ankit@ptotem.com>
2017-04-24 03:54:49 +0530

    printad renamed

--- app/models/print.rb
+++ app/models/print.rb
@@ -1,11 +1,11 @@
-class PrintAd
+class Print
 	include Mongoid::Document
 	include Mongoid::Timestamps
 
 	field :title, type: String
 	field :content, type: String
 
-	has_many :print_ad_images
+	has_many :print_images
 	
 
 end
--- app/models/print_image.rb
+++ app/models/print_image.rb
@@ -1,12 +1,12 @@
-class PrintAdImage
+class PrintImage
 	include Mongoid::Document
 	include Mongoid::Timestamps
 	include Mongoid::Paperclip
 
-	field :sequence, type: Integer, default: ->{PrintAdImage.count}
+	field :sequence, type: Integer, default: ->{PrintImage.count}
 	field :caption, type: String
 	has_mongoid_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage/
 
-	belongs_to :print_ad
+	belongs_to :print
 end
