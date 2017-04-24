cbd51ebfa9acdd8268f77a4299b727c7f67db282
Ankit <ankit@ptotem.com>
2017-04-24 10:49:12 +0530

    home page images slick wip

--- app/models/home_image.rb
+++ app/models/home_image.rb
@@ -7,6 +7,8 @@ class HomeImage
 	field :content, type: String
 	has_mongoid_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage/
+	has_mongoid_attached_file :portrait_image
+	validates_attachment_content_type :portrait_image, content_type: /\Aimage/
 
 	belongs_to :home
 end
